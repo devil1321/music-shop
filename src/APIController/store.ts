b6556302e57159d36e2681ac5366ba03f4e252a9
devil1321 <s.dominik1321@gmail.com>
2023-01-13 21:03:35 +0100

    server init

--- src/APIController/store.ts
+++ src/APIController/store.ts
@@ -4,11 +4,6 @@ import { createStore , applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/root.reducer'
 
-declare global {
-    interface Window {
-      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-    }
-  }
 
 const middleware = [thunk]
 
@@ -20,7 +15,6 @@ const preloadedState = () => {
         initState,
         compose(
             applyMiddleware(...middleware),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
         )
     )
 }
