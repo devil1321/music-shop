fbbc2d517295a63993f144246c04dfb61747a40e
devil1321 <s.dominik1321@gmail.com>
2023-01-17 18:40:28 +0100

    added pages desktop completed

--- src/APIController/store.ts
+++ src/APIController/store.ts
@@ -9,20 +9,11 @@ const middleware = [thunk]
 
 const initState  = {}
 
-
-declare global {
-    interface Window {
-      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-    }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const preloadedState = () => {
     return createStore(
         rootReducer,
         initState,
-        composeEnhancers(
+        compose(
             applyMiddleware(...middleware),
         )
     )
